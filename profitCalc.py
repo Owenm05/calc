f55adb037cbbd47bf21bcffaaaa789ba135b507c
Hun <43863985+Owenm05@users.noreply.github.com>
2023-01-03 12:59:13 -0600

    Create profitCalc.py

---
+++ profitCalc.py
@@ -0,0 +1,11 @@
+# Program to calculate profit
+
+# Ask user to enter cost price and selling price
+cost_price = float(input("Enter the cost price of the item: "))
+selling_price = float(input("Enter the selling price of the item: "))
+
+# Calculate the profit
+profit = selling_price - cost_price
+
+# Print the profit
+print("The profit is:", profit)
